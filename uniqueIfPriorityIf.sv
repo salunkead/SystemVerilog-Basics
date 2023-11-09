b05cca2cd5d38d2f301452499a24327850049353
Amol Salunke <salunkeamol10@gmail.com>
2023-11-09 15:24:32 +0530

    the concept of unique if and priority if

--- uniqueIfPriorityIf.sv
+++ uniqueIfPriorityIf.sv
@@ -1,6 +1,8 @@
+//unique if evaluates all the conditions parallel 
 //unique if will throw error/warning in following cases
 //more than one conditions matches in this condition: unique if will show warnning
 // No condition is true in the unique/priority if/case statement.
+//Example-1
 module test;
   int a,b,c;
   initial
@@ -17,6 +19,7 @@ module test;
     end
 endmodule
 
+//Example-2
 module test1;
   int a,b,c;
   initial
@@ -30,5 +33,23 @@ module test1;
         $display("b is less than c");//Warning: (vsim-8360) No condition is true in the unique/priority if/case statement.
       else
         $display("ghe");
+    end
+endmodule
+
+//priority if evaluates all the conditions in sequential order
+//priority if :- in the following condition simulator will show error/warnning
+//if no condition matches or there is missing else part then simulator will show warnning
+module test;
+  int a,b,c;
+  initial
+    begin
+      //Warning: (vsim-8315) No condition is true in the unique/priority if/case statement.
+      a=20;
+      b=10;
+      c=5;
+      priority if(a<b)
+        $display("a is less than b");
+      else if(b<c)
+        $display("b is less than c");
     end
 endmodule
