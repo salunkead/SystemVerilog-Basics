674bfe296ea09995c2664eed60c8d3d4d5be78cb
Amol Salunke <salunkeamol10@gmail.com>
2023-11-21 14:20:16 +0530

    difference between local and protected class properties

--- localVsProtected.sv
+++ localVsProtected.sv
@@ -1,1 +1,29 @@
 //the difference between local and protected can be demonstrated in inheritance
+class parent;
+  local int a;
+  protected int b;
+  function new(int a,b);
+    this.a=a;
+    this.b=b;
+  endfunction
+endclass
+class child extends parent;
+  function new(int x,y);
+    super.new(x,y);
+  endfunction
+  //the main difference between local and protected is that local properties are not inherited but protected does
+  //i.e we can access protected properties in child class but local properties can't
+  function void display();
+    //$display("local property of parent: a=%0d",a);
+    $display("protected property of parent: b=%0d",b);
+  endfunction
+endclass
+
+module test;
+  child c;
+  initial
+    begin
+      c=new(10,45);
+      c.display;
+    end
+endmodule
