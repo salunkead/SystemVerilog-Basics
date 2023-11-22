b5f004344056482b8f243e1ced9813fb62dcd9ae
Amol Salunke <salunkeamol10@gmail.com>
2023-11-22 18:38:30 +0530

    the concept of inline constraint

--- inLineConstraint.sv
+++ inLineConstraint.sv
@@ -1,5 +1,9 @@
 //inline constraint is used to introduce additional constraint on variable
 //randomize() with{constraint_expression} is called as inline constraint
+//constraint solver,see both constraint i.e constraint written inside class as well as inline constraint to solve the constraint
+//there should not be any conflict between class constraint and inline constraint,otherwise it will lead to randomization failure
+//existing constraint cannot be overriden with inline constraint
+
 class inLine;
   rand bit[7:0]a,b,c;
   function void post_randomize;
