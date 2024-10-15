1605c6ff3a4dd191a8482e9df44d467d90a3da07
Amol Salunke <salunkeamol10@gmail.com>
2024-10-15 14:14:13 +0530

    Generic interface with modport

---
+++ genericInterface.sv
@@ -0,0 +1,37 @@
+//Generic interface with modport
+
+//Example
+interface intf;
+  logic[31:0]a,b,y;
+  
+  modport adder(input a,b,output y);
+endinterface
+
+module design1(interface if1);  //Generic interface
+  always@(*)
+    begin
+      if1.y=if1.a+if1.b;
+    end
+endmodule
+
+module top;
+  intf vif();
+  design1 dut(vif.adder); //interface_instance.modport name
+  
+  initial
+    begin
+      repeat(10)
+        begin
+          vif.a=$urandom_range(1,50);
+          vif.b=$urandom_range(20,100);
+          #10;
+        end
+    end
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #500 $finish;
+    end
+endmodule
